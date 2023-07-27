f3889809bbbec161ef9cf7751887cf22d8fcf711
minyoung choi <sedcreta12@gmail.com>
2023-07-27 15:13:00 +0900

    기존 내용 백업

---
+++ MyApplication_kt/app/src/main/java/com/example/myapplication_kt/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.example.myapplication_kt
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+//        ------------------ Find ID ------------------
+        val myText:TextView = findViewById(R.id.ID_Textview_1)
+        val loginBt:Button = findViewById(R.id.bt_Login)
+        val signUp:Button = findViewById(R.id.bt_SignUp)
+
+        myText.text = "start Android by Kt"
+        
+        loginBt.setOnClickListener(){
+            Toast.makeText(this,"토스트 메시지 - LOGIN Click",Toast.LENGTH_SHORT).show()
+        }
+        
+        signUp.setOnClickListener(){
+            Toast.makeText(this, "토스트 메시지 - signUp", Toast.LENGTH_SHORT).show()
+//            test
+        }
+    }
+}
+
