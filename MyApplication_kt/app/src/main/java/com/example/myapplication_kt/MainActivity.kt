cf0ea6e562c4f0cfdb3069e602a90137d983fcc9
minyoung choi <sedcreta12@gmail.com>
2023-08-13 12:55:42 +0900

    AndroidX 적용해보기

--- MyApplication_kt/app/src/main/java/com/example/myapplication_kt/MainActivity.kt
+++ MyApplication_kt/app/src/main/java/com/example/myapplication_kt/MainActivity.kt
@@ -3,11 +3,17 @@ package com.example.myapplication_kt
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
 import android.util.Log
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.camera.view.PreviewView
+import androidx.core.content.ContextCompat
 
 
 class MainActivity : AppCompatActivity() {
@@ -36,23 +42,50 @@ class MainActivity : AppCompatActivity() {
         }
 
         myText.text = "start Android by Kt"
-        
+
+//        ################# CAMERAX Test #################
+        val preview = Preview.Builder().build()
+        val previewView = findViewById<PreviewView>(R.id.previewView)
+        val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+        preview.setSurfaceProvider(previewView.surfaceProvider)
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
+        cameraProviderFuture.addListener(Runnable {
+            val cameraProvider = cameraProviderFuture.get()
+            cameraProvider.unbindAll()
+            cameraProvider.bindToLifecycle(this, cameraSelector, preview)
+        }, ContextCompat.getMainExecutor(this))
+
+
+//        ################# CAMERAX Test #################
+
+
+
+
         loginBt.setOnClickListener {
             Toast.makeText(this, "토스트 메시지 - LOGIN Click + data:$deepLinkData", Toast.LENGTH_SHORT).show()
-//            if (true){
-//
-//            }
+            dispatchTakePictureIntent()
         }
 
 
         signUp.setOnClickListener{
             Toast.makeText(this, "토스트 메시지 - signUp", Toast.LENGTH_SHORT).show()
+
         }
 
         intent?.let{
             handleDeepLinkData(it)
         }
 
+    }
+
+    private val REQUEST_IMAGE_CAPTURE = 1
+
+
+    private fun dispatchTakePictureIntent(){
+        val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        if (takePictureIntent.resolveActivity(packageManager) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
+        }
     }
 
     private fun handleDeepLinkData(intent: Intent){
